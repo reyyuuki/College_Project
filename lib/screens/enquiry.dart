f7584b872de9b887a6fac2c05f6b03e620588995
Rey Yuuki <irfan31331@gmail.com>
2024-12-06 00:12:53 +0530

    build: added enquiry page

---
+++ lib/screens/enquiry.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Enquiry extends StatelessWidget {
+  const Enquiry({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("Enquiry Screen"),),
+    );
+  }
+}
