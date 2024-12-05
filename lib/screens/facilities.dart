85dc40389c3bad588859838feac6bd399cf1645d
Rey Yuuki <irfan31331@gmail.com>
2024-12-06 00:13:35 +0530

    build: added facilitiesi page

---
+++ lib/screens/facilities.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Facilities extends StatelessWidget {
+  const Facilities({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Facilities Screen"),
+      ),
+    );
+  }
+}
