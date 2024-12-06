c2e0d9a14104ebe1584ee32063272a6cc13ec684
Rey Yuuki <irfan31331@gmail.com>
2024-12-06 12:07:22 +0530

    feat: added infrastructure carousale

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,6 @@
-import 'package:agarwal_school/core/icons/icons_broken.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:agarwal_school/data/Scrapper/home_page_scrapper.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:shimmer/shimmer.dart';
 
 class HomePage extends StatefulWidget {
@@ -20,12 +18,6 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     getData();
   }
-
-  // List<Map<String, String>> schoolInfo = [
-  //   {'heading': 'Academic Excellence', 'description': 'Agrawal Public School focuses on delivering strong academic results through a comprehensive curriculum.'},
-  //   {'heading': 'Co-curricular Activities', 'description': 'The school offers a variety of extracurricular activities to help students develop creativity and confidence.'},
-  //   {'heading': 'Modern Infrastructure', 'description': 'Agrawal Public School is equipped with state-of-the-art facilities, including advanced classrooms, labs, and sports areas.'},
-  // ];
 
   void getData() async {
     final response = await Scrapper().homePageScrapper();
@@ -45,10 +37,6 @@ class _HomePageState extends State<HomePage> {
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         children: [
-          Animate(
-            effects: [FadeEffect(), ScaleEffect()],
-            child: const Text("Our Campus",textAlign: TextAlign.center,style: TextStyle(fontFamily: "LexendDeca",fontSize: 30),),
-          ),
           const SizedBox(height: 20),
           ConstrainedBox(
             constraints: const BoxConstraints(maxHeight: 200),
@@ -59,25 +47,103 @@ class _HomePageState extends State<HomePage> {
               children: List.generate(data!['images'].length, (index) {
                 final item = data!['images'][index];
                 return ClipRRect(
-                  borderRadius: const BorderRadius.vertical(
-                      top: Radius.circular(10)),
+                  borderRadius:
+                      const BorderRadius.vertical(top: Radius.circular(10)),
                   child: CachedNetworkImage(
                     imageUrl: item['image'],
                     fit: BoxFit.cover,
-                     placeholder: (context, url) =>
-                                Shimmer.fromColors(
-                              baseColor: Colors.grey[900]!,
-                              highlightColor: Colors.grey[700]!,
-                              child: Container(
-                                color: Colors.grey[400],
-                                height: 250,
-                                width: double.infinity,
-                              ),
-                            ),
+                    placeholder: (context, url) => Shimmer.fromColors(
+                      baseColor: Colors.grey[900]!,
+                      highlightColor: Colors.grey[700]!,
+                      child: Container(
+                        color: Colors.grey[400],
+                        height: 250,
+                        width: double.infinity,
+                      ),
+                    ),
                     errorWidget: (context, error, stackTrace) {
-                      return const Icon(Broken.archive_1);
+                      return const Icon(Icons.error);
                     },
                   ),
+                );
+              }),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.surfaceContainerHighest,
+                border: Border.all(
+                    width: 1, color: Theme.of(context).colorScheme.primary),
+                borderRadius: BorderRadius.circular(20)),
+            child: const Text(
+              "Infrastructure & Facilities",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontFamily: "LexendDeca", fontSize: 30),
+            ),
+          ),
+          const SizedBox(height: 10,),
+          ConstrainedBox(
+            constraints: const BoxConstraints(maxHeight: 300),
+            child: CarouselView(
+              itemExtent: MediaQuery.of(context).size.width,
+              shrinkExtent: 100,
+              itemSnapping: true,
+              children: List.generate(data!['facilities'].length, (index) {
+                final item = data!['facilities'][index];
+                return Column(
+                  children: [
+                    Container(
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+                      ),
+                      height: 150,
+                      width: MediaQuery.of(context).size.width,
+                      child: ClipRRect(
+                        borderRadius:
+                            const BorderRadius.vertical(top: Radius.circular(10)),
+                        child: CachedNetworkImage(
+                          imageUrl: item['image'] ?? "",
+                          fit: BoxFit.cover,
+                          placeholder: (context, url) => Shimmer.fromColors(
+                            baseColor: Colors.grey[900]!,
+                            highlightColor: Colors.grey[700]!,
+                            child: Container(
+                              color: Colors.grey[400],
+                              height: 250,
+                              width: double.infinity,
+                            ),
+                          ),
+                          errorWidget: (context, error, stackTrace) {
+                            return const Icon(Icons.error);
+                          },
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 140,
+                      padding: const EdgeInsets.all(10),
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.surfaceContainerHigh,
+                        borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(item['title'], style: const TextStyle(fontFamily: "LexendDeca",fontSize: 18),),
+                          const SizedBox(height: 10,),
+                          Text(item['description'])
+                        ],
+                      ),
+                    )
+                  ],
                 );
               }),
             ),
